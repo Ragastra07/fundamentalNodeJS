32a3a019ad0de243be71c3311065b983a63124d1
AGA(ノ｀Д´)ノ彡┻━┻ <haryo.accn.7@gmail.com>
2024-09-12 19:08:39 +0700

    Debuger-node library

---
+++ libDEBUGER.mjs
@@ -0,0 +1,9 @@
+function sayHallo(name){
+    debugger;
+    return `hallo ${name}`;
+}
+
+const name = "tara";
+debugger;
+// console.info(sayHallo(null)); //ketika salah
+console.info(sayHallo(name));
